a1d6e29a0daddeb39d2297ca8a9ef0e0493999c0
tomorek-2 <omavrina89@gmail.com>
2026-08-21 18:03:28 +0300

    Se

--- arcCpp/Events.cpp
+++ arcCpp/Events.cpp
@@ -2,31 +2,29 @@
 #include "map"
 #include <functional>
 #include <string>
+#include "arcCpp/struct/Seq.hpp"
 //Я не могу написать Seq, ObjectMap в один день с событиями.
 namespace arc {
 std::function<void()> Events::events[500];
+ arc::structures::Seq<std::function<void()>> eventsSeq;
 std::string Events::names[500];
+    arc::structures::Seq<std::string> stringSeq;
 void Events::on(const std::function<void()>& callbackF, std::string name) {
-    for (int i = 0; i < 500; i++) {
+   /* for (int i = 0; i < 500; i++) {
         if(names[i] == name) return;
         if(!events[i]) {
             events[i] = callbackF;
             names[i] = name;
             return;
         }
-    }
+    } */
+eventsSeq.add(callbackF);
+stringSeq.add(name);
+    return;
 };
 void Events::fire(std::string name) {
-    int i = 0;
-    for(std::string nameI : names) {
 
-        if(name == nameI) {
-            std::function<void()> event = events[i];
-            event();
-            return;
-        }
-        i++;
-    }
+
 };
 
 void Events::remove(std::string name) {
@@ -44,6 +42,7 @@ void Events::remove(std::string name) {
 
 
 };
+
 }
 
 
--- arcCpp/struct/Seq.hpp
+++ arcCpp/struct/Seq.hpp
@@ -1,15 +1,79 @@
 #ifndef ARC_STRUCT_SEQ_HPP
 #define ARC_STRUCTURE_SEQ_HPP
-
+#include <string>
 namespace arc::structures {
 
     template <typename T>
     class Seq {
     public:
-        void resize(int newSize);
-        T items[500];
-        bool contains(T items);
+        int length = 500;
+
+
+        T* items = nullptr;
+        int freeSpace = 0;
+        bool contains(T itemC) {
+            if(items == nullptr) createArray(length);
+            for(T item : items) {
+                if(itemC == item) return true;
+            }
+            return false;
+
+
+        };
+
+        void add(T item)  {
+            if(items == nullptr) createArray(length);
+            while(true) {
+                if(freeSpace >= length) {
+                    length *= 2;
+                    createArray(length);
+                }
+                items[freeSpace] = item;
+                freeSpace++;
+                return;
+
+
+            }
+        };
+        T get(int key)  {
+            while(true) {
+
+                if (items == nullptr) {createArray(length);}
+                else
+                    return items[key];
+
+            }
+
+        };
+         int get(T key)  {
+             if(items == nullptr) createArray(length);
+             int i = 0;
+             for(T item : items) {
+
+
+                 if(key == item) return i++;
+                 i++;
+             }
+
+             return 0;
+
+
+
+         };
+    private:
+        void createArray(int size) {
+          T* newItems = new T[size];
+          delete[] items;
+          items = newItems;
+          freeSpace = 0;
+
+
+        };
+
+
     };
+
+
 
 }
 #endif
